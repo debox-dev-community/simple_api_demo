eb027955fafb5c86ba2eccb7c4cf7d49bc4cd62d
debox_tq <doupi520520@gmail.com>
2023-06-14 20:52:15 +0800

    feat(): add project

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# simple_api_demo
+# A Simple DeBox API Example
+
+**English** | [中文](./README.zh_CN.md)
+
+## Setting up the Vue Project
+
+### Overview
+
+1. Modify vue.config.js to handle cross-origin requests.
+2. Create an API endpoint to fetch Group information and configure the API-Key. 
+3. Add an input field on the frontend page to receive the 'group_invite_url' parameter.
+4. Add a button on the frontend page to trigger the API call and fetch the Group information.
+
+
+### Getting Started
+
+1. Replace the API-Key in getGroupInfo.js.
+
+2. Start the project:
+```
+npm run serve
+```
+
+3. Once the page is open, enter any Group's sharing link in the input field. You can use this link for testing 
+```
+https://debox.space/dao/wC6DlLGJ 
+```
+
+![](./public/shot1.png)
+
+4. After entering the link, click the button, and a panel will pop up displaying the Group's information.
+
+![](./public/shot2.png)
+
+### Join our official developer community and participate in ecosystem development!！
+ [Click to Join](https://debox.space/dao/wC6DlLGJ)
+
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello"> 
+    <div >
+      <input id="sty" v-model="input" placeholder="Input group share url" />
+    </div>
+   
+    <div>
+      <button id="sty" @click="btnclick">Get group info</button>
+    </div>
+    
+    <div>
+      <span id="sty" >{{ result }}</span>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import getGroupInfo from "../api/getGroupInfo";
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      input: "",
+      result:''
+    };
+  },
+  methods: { 
+    async btnclick() {
+      // DeBox开发者社区:  https://debox.space/dao/wC6DlLGJ
+      const res = await getGroupInfo(this.input);
+      // 正常返回数据
+      if(res.code === 200) { 
+        // group_name
+        // group_number
+        this.result= ` ${res.data.group_name} : ${res.data.group_number} members`
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+#sty {
+  width: 200px; height: 30px;
+  text-align: center;
+}
+</style>
